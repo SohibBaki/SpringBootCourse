545c507fb057a55aef35d21329a9392987dd0ece
SohibBaki <SohibBaki@users.noreply.github.com>
2025-08-25 22:17:31 +0300

    Path Variabales - Part 02

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -3,6 +3,7 @@ package com.luv2code.demo.rest;
 import com.luv2code.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +31,17 @@ public class StudentRestController {
     public List<Student> getStudents(){
         return theStudents;
     }
+
+    // define endpoints or "/students/{studentId}" - return student at index
+
+    @GetMapping ("/student/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+
+        // just index into the list ... keep it simple for now
+        return theStudents.get(studentId) ;
+    }
+
+
 }
 
 
