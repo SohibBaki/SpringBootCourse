762e33c15f995f0ed064acb2f698625a8f54573a
SohibBaki <SohibBaki@users.noreply.github.com>
2025-09-22 21:18:21 +0300

    REST API Security - JDBC Authentication - Plain Text - Part 01 -

--- 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -3,18 +3,25 @@ package com.luv2code.springboot.cruddemo.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class DemoSecurityConfig {
+
+    // add support for JDBC ... NO MORE HARDCODED USERS :-)
+
     @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+
+        return new JdbcUserDetailsManager(dataSource);
+    }
+   /* @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
 
         UserDetails john = User.builder()
@@ -37,6 +44,7 @@ public class DemoSecurityConfig {
 
         return new InMemoryUserDetailsManager (john, mary ,susan);
     }
+    */
     @Bean
     public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
 
