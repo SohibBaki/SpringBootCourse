620afa601b1a5b62e411350aed3c6a6e7741ff28
SohibBaki <SohibBaki@users.noreply.github.com>
2025-09-25 21:42:21 +0300

    Spring MVC with Thymeleaf- Part 01 -

---
+++ 06-spring-boot-mvc/01-thymeleafdemo-mvc/src/main/java/com/luv2code/springboot/thymeleafdemoo/ThymeleafdemooApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.thymeleafdemoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafdemooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafdemooApplication.class, args);
+	}
+
+}
