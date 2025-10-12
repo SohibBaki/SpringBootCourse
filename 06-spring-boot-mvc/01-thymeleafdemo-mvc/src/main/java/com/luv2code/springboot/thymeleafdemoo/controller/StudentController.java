b49b7f17e21e4bc31307d65ff9636a3e4376fa75
SohibBaki <SohibBaki@users.noreply.github.com>
2025-10-12 21:30:29 +0300

    Spring MVC Form Data Binding - Part 03 -

--- 06-spring-boot-mvc/01-thymeleafdemo-mvc/src/main/java/com/luv2code/springboot/thymeleafdemoo/controller/StudentController.java
+++ 06-spring-boot-mvc/01-thymeleafdemo-mvc/src/main/java/com/luv2code/springboot/thymeleafdemoo/controller/StudentController.java
@@ -4,6 +4,10 @@ import com.luv2code.springboot.thymeleafdemoo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.sql.SQLOutput;
 
 @Controller
 public class StudentController {
@@ -18,5 +22,18 @@ public class StudentController {
         theModel.addAttribute("student",theStudent);
 
         return "student-form";
+
     }
+
+@PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent){
+
+        // log the input data
+    System.out.println("theStudent :" + theStudent.getFirstName() + " " + theStudent.getLastName());
+        return "student-confirmation";
+}
+
+
+
+
 }
