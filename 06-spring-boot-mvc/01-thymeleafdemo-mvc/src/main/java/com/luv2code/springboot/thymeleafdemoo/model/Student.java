017ff9f569f7888f995b24d2aaa794b3db3f525b
SohibBaki <SohibBaki@users.noreply.github.com>
2025-10-26 21:46:11 +0300

    Spring MVC Data Binding - Check Boxe - Part 01 -

--- 06-spring-boot-mvc/01-thymeleafdemo-mvc/src/main/java/com/luv2code/springboot/thymeleafdemoo/model/Student.java
+++ 06-spring-boot-mvc/01-thymeleafdemo-mvc/src/main/java/com/luv2code/springboot/thymeleafdemoo/model/Student.java
@@ -1,10 +1,13 @@
 package com.luv2code.springboot.thymeleafdemoo.model;
 
+import java.util.List;
+
 public class Student {
     private String firstName;
     private String lastName;
     private String country;
     private String favoriteLanguage;
+    private List<String> favoriteSystems;
     public Student(){
 
     }
@@ -39,6 +42,14 @@ public class Student {
 
     public void setFavoriteLanguage(String favoriteLanguage) {
         this.favoriteLanguage = favoriteLanguage;
+    }
+
+    public List<String> getFavoriteSystems() {
+        return favoriteSystems;
+    }
+
+    public void setFavoriteSystems(List<String> favoriteSystems) {
+        this.favoriteSystems = favoriteSystems;
     }
 }
 
